3551adff6d0522882c33cec35cfee0b875ff15a9
SimonP35 <simon.pejac@gmail.com>
2021-09-15 20:32:56 +0200

    wip-Encore.webpack

--- start/webpack.config.js
+++ start/webpack.config.js
@@ -35,12 +35,12 @@ Encore
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
-    .
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     // .enableSingleRuntimeChunk()
     .disableSingleRuntimeChunk()
+
 
     /*
      * FEATURE CONFIG
@@ -66,7 +66,12 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
